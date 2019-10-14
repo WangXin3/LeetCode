3d2b0625aecfcf4e1590a4f0c84f729d272265ef
wangx <91907@163.com>
2019-10-14 15:23:09 +0800

    逆序打印链表

--- src/PrintListFromTailToHead.java
+++ src/PrintListFromTailToHead.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * @author 她爱微笑
  * @date 2019/10/14
@@ -5,7 +8,77 @@
  */
 public class PrintListFromTailToHead {
 
+	public static void main(String[] args) {
+		ListNode<Integer> head = new ListNode<>(1);
+		head.next = new ListNode<>(2);
+		head.next.next = new ListNode<>(3);
+		head.next.next.next = new ListNode<>(4);
+		System.out.println("head = " + head);
+//		printReversinglyRecursively(head);
+		printListFromTailToHeadByStack(head);
+	}
 
+	/**
+	 * 递归版
+	 * @param node
+	 */
+	public static void printReversinglyRecursively(ListNode<Integer> node) {
+		if (node == null) {
+			return;
+		} else {
+			printReversinglyRecursively(node.next);
+			System.out.println(node.val);
+		}
+	}
 
+	/**
+	 * 使用栈
+	 * 栈具有先进后出的热点
+	 * @param node
+	 */
+	public static void printListFromTailToHeadByStack(ListNode<Integer> node) {
+		Stack<Integer> stack = new Stack();
 
+		while (node != null) {
+			stack.add(node.val);
+			node = node.next;
+		}
+
+		ArrayList<Integer> array = new ArrayList<>();
+		while (!stack.isEmpty()) {
+			array.add(stack.pop());
+		}
+
+		for (Integer i : array) {
+			System.out.println(i);
+		}
+	}
+
+}
+
+class ListNode<T> {
+	public T val;
+	public ListNode<T> next;
+
+	public ListNode(T val) {
+		this.val = val;
+		this.next = null;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder ret = new StringBuilder();
+		ret.append("[");
+		for (ListNode cur = this; ; cur = cur.next) {
+			if (cur == null) {
+				ret.deleteCharAt(ret.lastIndexOf(" "));
+				ret.deleteCharAt(ret.lastIndexOf(","));
+				break;
+			}
+			ret.append(cur.val);
+			ret.append(", ");
+		}
+		ret.append("]");
+		return ret.toString();
+	}
 }
