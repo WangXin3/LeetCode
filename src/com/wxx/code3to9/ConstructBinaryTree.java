7bc0584d7d9622eea6278ecee0b152ca0263115d
wangx <91907@163.com>
2019-10-18 17:31:51 +0800

    重建二叉树

---
+++ src/com/wxx/code3to9/ConstructBinaryTree.java
@@ -0,0 +1,66 @@
+package com.wxx.code3to9;
+
+import com.wxx.utils.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 她爱微笑
+ * @date 2019/10/18
+ * 构建二叉树
+ */
+public class ConstructBinaryTree {
+
+	/**
+	 * 缓存中序遍历数组每个值对应的索引
+	 */
+	private Map<Integer, Integer> indexForInOrders = new HashMap<>();
+
+	/**
+	 * 构建二叉树
+	 * @param pre 前序遍历数组
+	 * @param in 中序遍历数组
+	 * @return
+	 */
+	public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+		for (int i = 0; i < in.length; i++) {
+			indexForInOrders.put(in[i], i);
+		}
+
+		return reConstructBinaryTree(pre, 0, pre.length - 1, 0);
+	}
+
+	/**
+	 *
+	 * @param pre
+	 * @param preL
+	 * @param preR
+	 * @param inL
+	 * @return
+	 */
+	private TreeNode reConstructBinaryTree(int[] pre, int preL, int preR, int inL) {
+		if (preL > preR) {
+			return null;
+		}
+
+		TreeNode root = new TreeNode(pre[preL]);
+		int inIndex = indexForInOrders.get(root.val);
+		int leftTreeSize = inIndex - inL;
+		root.left = reConstructBinaryTree(pre, preL + 1, preL + leftTreeSize, inL);
+		root.right = reConstructBinaryTree(pre, preL + leftTreeSize + 1, preR, inL + leftTreeSize + 1);
+		return root;
+	}
+
+	public static void main(String[] args) {
+		int[] pre = {3, 9, 20, 15, 7};
+		int[] in = {9, 3, 15, 20, 7};
+
+		ConstructBinaryTree constructBinaryTree = new ConstructBinaryTree();
+		TreeNode treeNode = constructBinaryTree.reConstructBinaryTree(pre, in);
+		System.out.println("treeNode = " + treeNode);
+
+
+	}
+
+}
