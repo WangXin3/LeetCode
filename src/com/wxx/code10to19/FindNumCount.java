3f91a030a5224faa9e40bf2669238e3dad8c228b
wangxinxin <91907@163.com>
2020-01-21 10:06:13 +0800

    找二进制中1的个数

--- src/com/wxx/code10to19/FindNumCount.java
+++ src/com/wxx/code10to19/FindNumCount.java
@@ -6,7 +6,12 @@ package com.wxx.code10to19;
  * 输入一个数，将数转为二进制，计算出其中1的个数
  */
 public class FindNumCount {
-
+	/**
+	 * 左移方法
+	 *
+	 * @param n
+	 * @return
+	 */
 	public int numberOf1(int n) {
 		int count = 0;
 		int flag = 1;
@@ -22,9 +27,29 @@ public class FindNumCount {
 		return count;
 	}
 
+	/**
+	 * 原数减1与原数做 & 运算，可以得出去掉最右边1
+	 * @param n
+	 * @return
+	 */
+	public int numberOf1B(int n) {
+		int count = 0;
+		while (n != 0) {
+			count++;
+
+			n = (n - 1) & n;
+		}
+		return count;
+	}
+
+
 	public static void main(String[] args) {
 		FindNumCount findNumCount = new FindNumCount();
 		int i = findNumCount.numberOf1(170);
+		int i1 = findNumCount.numberOf1B(170);
 		System.out.println("i = " + i);
+		System.out.println("i1 = " + i1);
+		int i2 = Integer.bitCount(170);
+		System.out.println("i2 = " + i2);
 	}
 }
