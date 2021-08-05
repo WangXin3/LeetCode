fdefdbb80b3bfff01e61c95b8da5b93ced04c315
wangxinxin <Xinxin.Wang@cdpgroupltd.com>
2021-08-05 10:00:30 +0800

    Day05.java

---
+++ src/com/wxx/random/aug/Day05.java
@@ -0,0 +1,50 @@
+package com.wxx.random.aug;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Xinxin.Wang
+ * @since 2021/8/5 9:19
+ */
+public class Day05 {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        List<List<Integer>> rg = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n; ++i) {
+            rg.add(new ArrayList<>());
+        }
+        int[] inDeg = new int[n];
+        for (int x = 0; x < n; ++x) {
+            for (int y : graph[x]) {
+                rg.get(y).add(x);
+            }
+            inDeg[x] = graph[x].length;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            if (inDeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int y = queue.poll();
+            for (int x : rg.get(y)) {
+                if (--inDeg[x] == 0) {
+                    queue.offer(x);
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            if (inDeg[i] == 0) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
