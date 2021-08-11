cd541ab620f032550d41e4c7f0b82fa27d530ec9
wangxinxin <Xinxin.Wang@cdpgroupltd.com>
2021-08-11 09:20:35 +0800

    Day11.java

---
+++ src/com/wxx/random/aug/Day11.java
@@ -0,0 +1,29 @@
+package com.wxx.random.aug;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Xinxin.Wang
+ * @since 2021/8/11 9:19
+ */
+public class Day11 {
+
+    public int numberOfArithmeticSlices(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        Map<Long, Integer>[] f = new Map[n];
+        for (int i = 0; i < n; ++i) {
+            f[i] = new HashMap<>();
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long d = 1L * nums[i] - nums[j];
+                int cnt = f[j].getOrDefault(d, 0);
+                ans += cnt;
+                f[i].put(d, f[i].getOrDefault(d, 0) + cnt + 1);
+            }
+        }
+        return ans;
+    }
+}
