536702dd9f66790ae94528e4a6e1c1950c40ffcb
wangxinxin <Xinxin.Wang@cdpgroupltd.com>
2021-09-23 09:40:09 +0800

    Day23.java

---
+++ src/com/wxx/random/sep/Day23.java
@@ -0,0 +1,19 @@
+package com.wxx.random.sep;
+
+/**
+ * @author Xinxin.Wang
+ * @since 2021/9/23 9:17
+ */
+public class Day23 {
+
+    public boolean isPowerOfThree(int n) {
+        while (n != 0 && n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
