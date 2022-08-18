547590906912cf631199f7b69a8437d2b38e10c0
wangxinxin <Xinxin.Wang@cdpgroupltd.com>
2022-08-18 10:56:19 +0800

    num022

--- src/com/wxx/top100/Num022.java
+++ src/com/wxx/top100/Num022.java
@@ -9,23 +9,44 @@ import java.util.List;
  */
 public class Num022 {
     public static void main(String[] args) {
-        // ()
-        // ()() (())
-        // ()()() ()(()) (())() ((())) (()())
-        // ()()()() ()()(()) ()(())() (())()() ()((())) ((()))() ()(()()) (()())() (((()))) (()()()) ((()()))
+        Solution022 solution022 = new Solution022();
+        List<String> list = solution022.generateParenthesis(3);
+        list.forEach(System.out::println);
     }
 }
 
-class Solution {
+class Solution022 {
     public List<String> generateParenthesis(int n) {
-        char left = '(';
-        char right = ')';
+        List<String> combinations = new ArrayList<>();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
 
-        for (int i = 0; i < n; i++) {
-
+    public void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current)) {
+                result.add(new String(current));
+            }
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos + 1, result);
+            current[pos] = ')';
+            generateAll(current, pos + 1, result);
         }
+    }
 
-
-        return new ArrayList<>();
+    public boolean valid(char[] current) {
+        int balance = 0;
+        for (char c : current) {
+            if (c == '(') {
+                ++balance;
+            } else {
+                --balance;
+            }
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
     }
 }
