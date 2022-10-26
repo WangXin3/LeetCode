eb827240e3369e8c7232773adab27a7355ca405d
wangxinxin <Xinxin.Wang@cdpgroupltd.com>
2022-10-26 09:32:08 +0800

    Day26

---
+++ src/com/wxx/random/year2022/october/Day26.java
@@ -0,0 +1,38 @@
+package com.wxx.random.year2022.october;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author Xinxin.Wang
+ * @since 2022/10/26 9:29
+ */
+public class Day26 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int shortestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        long[] preSumArr = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            preSumArr[i + 1] = preSumArr[i] + nums[i];
+        }
+        int res = n + 1;
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i <= n; i++) {
+            long curSum = preSumArr[i];
+            while (!queue.isEmpty() && curSum - preSumArr[queue.peekFirst()] >= k) {
+                res = Math.min(res, i - queue.pollFirst());
+            }
+            while (!queue.isEmpty() && preSumArr[queue.peekLast()] >= curSum) {
+                queue.pollLast();
+            }
+            queue.offerLast(i);
+        }
+        return res < n + 1 ? res : -1;
+    }
+}
+
