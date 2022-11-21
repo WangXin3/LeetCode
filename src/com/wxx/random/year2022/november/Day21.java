2e857c08b2b84e5417fae6cd593df41dd754b806
wangxinxin <Xinxin.Wang@cdpgroupltd.com>
2022-11-21 09:56:26 +0800

    Day21

---
+++ src/com/wxx/random/year2022/november/Day21.java
@@ -0,0 +1,32 @@
+package com.wxx.random.year2022.november;
+
+/**
+ * @author Xinxin.Wang
+ * @since 2022/11/21 9:42
+ */
+public class Day21 {
+    public static void main(String[] args) {
+        Solution21 solution21 = new Solution21();
+        System.out.println(solution21.soupServings(1023));
+    }
+}
+
+class Solution21 {
+    public double soupServings(int n) {
+        n = (int) Math.ceil((double) n / 25);
+        if (n >= 179) {
+            return 1.0;
+        }
+        double[][] dp = new double[n + 1][n + 1];
+        dp[0][0] = 0.5;
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = 1.0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[Math.max(0, i - 4)][j] + dp[Math.max(0, i - 3)][Math.max(0, j - 1)] + dp[Math.max(0, i - 2)][Math.max(0, j - 2)] + dp[Math.max(0, i - 1)][Math.max(0, j - 3)]) / 4.0;
+            }
+        }
+        return dp[n][n];
+    }
+}
