87119c154cbe54a4e57b571655e832684b805e72
wangxinxin <Xinxin.Wang@cdpgroupltd.com>
2022-11-22 09:46:51 +0800

    Day22

--- src/com/wxx/random/year2022/november/Day22.java
+++ src/com/wxx/random/year2022/november/Day22.java
@@ -14,22 +14,19 @@ class Solution22 {
     static final int MOD = 1000000007;
 
     public int nthMagicalNumber(int n, int a, int b) {
+        long l = Math.min(a, b);
+        long r = (long) n * Math.min(a, b);
         int c = lcm(a, b);
-        int m = c / a + c / b - 1;
-        int r = n % m;
-        int res = (int) ((long) c * (n / m) % MOD);
-        if (r == 0) {
-            return res;
-        }
-        int addA = a, addB = b;
-        for (int i = 0; i < r - 1; ++i) {
-            if (addA < addB) {
-                addA += a;
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            long cnt = mid / a + mid / b - mid / c;
+            if (cnt >= n) {
+                r = mid - 1;
             } else {
-                addB += b;
+                l = mid + 1;
             }
         }
-        return (res + Math.min(addA, addB) % MOD) % MOD;
+        return (int) ((r + 1) % MOD);
     }
 
     public int lcm(int a, int b) {
